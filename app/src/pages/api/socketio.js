c1d46d0550695dd5fe55bd882f0f140124a821a3
hussam-aldarwish <hussam.aldarwish@hotmail.com>
2021-12-26 23:26:31 +0300

    test15

--- app/src/pages/api/socketio.js
+++ app/src/pages/api/socketio.js
@@ -11,13 +11,9 @@ async function handle(req, res) {
     console.log("New Socket.io server...");
     // adapt Next's net Server to http Server
     const httpServer = res.socket.server;
-    console.log(
-      "🚀 ~ file: socketio.js ~ line 14 ~ handle ~ httpServer",
-      httpServer.connection
-    );
     const io = new ServerIO(httpServer, {
       path: "/api/socketio",
-      port: 443,
+      transports: ["polling", "flashsocket"],
     });
     // append SocketIO server to Next.js socket server response
     res.socket.server.io = io;
--- app/src/pages/chat.js
+++ app/src/pages/chat.js
@@ -20,8 +20,7 @@ function Chat() {
     console.log(process.env.NEXT_PUBLIC_BASE_URL);
     const socket = SocketIOClient.connect(process.env.NEXT_PUBLIC_BASE_URL, {
       path: "/api/socketio",
-      requestCert: false,
-      rejectUnauthorized: false,
+      transports: ["polling", "flashsocket"],
     });
 
     socket.on("connect", () => {
